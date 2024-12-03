0dc15b3440cb9d62456009fe227495856fff5ced
Sthewerson <fernandoo-rocha@hotmail.com>
2024-12-03 08:32:42 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, world!!");
+import express from "express"
+
+const app = express()
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Listening running on port ${port}`)
+})
+
+app.get("/", (req, res) => {
+  res.send("Hello world")
+})
